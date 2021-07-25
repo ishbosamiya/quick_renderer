498dfdd705eb3bf0cfebfebd91283919e279d769
ishbosamiya <ishbosamiya@gmail.com>
2021-07-25 17:45:38 +0530

    example: simple_render: basic setup to render label "hello world!"

--- examples/simple_render.rs
+++ examples/simple_render.rs
@@ -1,3 +1,68 @@
+use egui::{FontDefinitions, FontFamily, TextStyle};
+use egui_glfw::EguiBackend;
+use glfw::Context;
+
+use quick_renderer::egui;
+use quick_renderer::egui_glfw;
+use quick_renderer::glfw;
+use quick_renderer::glm;
+
 fn main() {
-    println!("Hello world!");
+    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+
+    // creating window
+    let (mut window, events) = glfw
+        .create_window(1280, 720, "Simple Render", glfw::WindowMode::Windowed)
+        .expect("ERROR: glfw window creation failed");
+
+    // setup bunch of polling data
+    window.set_key_polling(true);
+    window.set_cursor_pos_polling(true);
+    window.set_mouse_button_polling(true);
+    window.set_framebuffer_size_polling(true);
+    window.set_scroll_polling(true);
+    window.set_char_polling(true);
+    window.make_current();
+
+    // setup the egui backend
+    let mut egui = EguiBackend::new(&mut window, &mut glfw);
+
+    let mut fonts = FontDefinitions::default();
+    // larger text
+    fonts
+        .family_and_size
+        .insert(TextStyle::Button, (FontFamily::Proportional, 18.0));
+    fonts
+        .family_and_size
+        .insert(TextStyle::Body, (FontFamily::Proportional, 18.0));
+    fonts
+        .family_and_size
+        .insert(TextStyle::Small, (FontFamily::Proportional, 15.0));
+    egui.get_egui_ctx().set_fonts(fonts);
+
+    while !window.should_close() {
+        glfw.poll_events();
+
+        glfw::flush_messages(&events).for_each(|(_, event)| {
+            egui.handle_event(&event, &window);
+        });
+
+        unsafe {
+            gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
+        }
+
+        // GUI starts
+        {
+            egui.begin_frame(&window, &mut glfw);
+            egui::CentralPanel::default().show(egui.get_egui_ctx(), |ui| {
+                ui.label("Hello World, Simple Render!");
+            });
+            let (width, height) = window.get_framebuffer_size();
+            let _output = egui.end_frame(glm::vec2(width as _, height as _));
+        }
+        // GUI ends
+
+        // Swap front and back buffers
+        window.swap_buffers();
+    }
 }
