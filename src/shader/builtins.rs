b388ef59a05809b81f79d90bee3951bedcfb11cf
ishbosamiya <ishbosamiya@gmail.com>
2021-07-27 22:22:27 +0530

    shader: builtin: smooth color 3D

--- src/shader/builtins.rs
+++ src/shader/builtins.rs
@@ -9,6 +9,12 @@ lazy_static! {
             get_directional_light_frag_code(),
         )
     };
+    static ref SMOOTH_COLOR_3D: Result<Shader, ShaderError> = {
+        Shader::from_strings(
+            get_smooth_color_3d_vert_code(),
+            get_smooth_color_3d_frag_code(),
+        )
+    };
 }
 
 pub fn get_directional_light_vert_code() -> &'static str {
@@ -21,4 +27,16 @@ pub fn get_directional_light_frag_code() -> &'static str {
 
 pub fn get_directional_light_shader() -> &'static Result<Shader, ShaderError> {
     &DIRECTIONAL_LIGHT
+}
+
+pub fn get_smooth_color_3d_vert_code() -> &'static str {
+    include_str!("../../shaders/shader_3D_smooth_color.vert")
+}
+
+pub fn get_smooth_color_3d_frag_code() -> &'static str {
+    include_str!("../../shaders/shader_3D_smooth_color.frag")
+}
+
+pub fn get_smooth_color_3d_shader() -> &'static Result<Shader, ShaderError> {
+    &SMOOTH_COLOR_3D
 }
