bce346f511c1c60c357090102d3b968a8f213b49
ishbosamiya <ishbosamiya@gmail.com>
2021-09-22 16:45:45 +0530

    gpu_utils: extract plane vertices to be lazy static

--- src/gpu_utils.rs
+++ src/gpu_utils.rs
@@ -1,9 +1,11 @@
+use lazy_static::lazy_static;
+
 use crate::glm;
 use crate::gpu_immediate::{GPUImmediate, GPUPrimType, GPUVertCompType, GPUVertFetchMode};
 use crate::shader::Shader;
 
-pub fn render_quad(imm: &mut GPUImmediate, shader: &Shader) {
-    let plane_vert_positions = vec![
+lazy_static! {
+    static ref PLANE_VERT_LIST_F32: Vec<(glm::Vec3, glm::Vec2)> = vec![
         (glm::vec3(1.0, 1.0, 0.0), glm::vec2(1.0, 1.0)),
         (glm::vec3(-1.0, -1.0, 0.0), glm::vec2(0.0, 0.0)),
         (glm::vec3(-1.0, 1.0, 0.0), glm::vec2(0.0, 1.0)),
@@ -11,7 +13,25 @@ pub fn render_quad(imm: &mut GPUImmediate, shader: &Shader) {
         (glm::vec3(1.0, 1.0, 0.0), glm::vec2(1.0, 1.0)),
         (glm::vec3(1.0, -1.0, 0.0), glm::vec2(1.0, 0.0)),
     ];
+    static ref PLANE_VERT_LIST_F64: Vec<(glm::DVec3, glm::DVec2)> = vec![
+        (glm::vec3(1.0, 1.0, 0.0), glm::vec2(1.0, 1.0)),
+        (glm::vec3(-1.0, -1.0, 0.0), glm::vec2(0.0, 0.0)),
+        (glm::vec3(-1.0, 1.0, 0.0), glm::vec2(0.0, 1.0)),
+        (glm::vec3(-1.0, -1.0, 0.0), glm::vec2(0.0, 0.0)),
+        (glm::vec3(1.0, 1.0, 0.0), glm::vec2(1.0, 1.0)),
+        (glm::vec3(1.0, -1.0, 0.0), glm::vec2(1.0, 0.0)),
+    ];
+}
 
+pub fn get_plane_vert_list_f32() -> &'static Vec<(glm::Vec3, glm::Vec2)> {
+    &PLANE_VERT_LIST_F32
+}
+
+pub fn get_plane_vert_list_f64() -> &'static Vec<(glm::DVec3, glm::DVec2)> {
+    &PLANE_VERT_LIST_F64
+}
+
+pub fn render_quad(imm: &mut GPUImmediate, shader: &Shader) {
     let format = imm.get_cleared_vertex_format();
     let pos_attr = format.add_attribute(
         "in_pos\0".to_string(),
@@ -28,7 +48,7 @@ pub fn render_quad(imm: &mut GPUImmediate, shader: &Shader) {
 
     imm.begin(GPUPrimType::Tris, 6, shader);
 
-    plane_vert_positions.iter().for_each(|(pos, uv)| {
+    get_plane_vert_list_f32().iter().for_each(|(pos, uv)| {
         imm.attr_2f(uv_attr, uv[0], uv[1]);
         imm.vertex_3f(pos_attr, pos[0], pos[1], pos[2]);
     });
